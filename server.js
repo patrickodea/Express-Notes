9d43681b9998625733843fd4b49b9e5b66ecde18
Patrick O'Dea <po39dea@gmail.com>
2023-10-05 21:52:33 -0600

    Setting up structure and starting server.js

---
+++ server.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 3001;
+
+app.use(express.static("public"));
+
+app.get("/", (req, res) =>
+ res.sendFile(path.join())
+
+)
+
+app.listen(PORT, () => 
+console.log("hello")
+
+);
