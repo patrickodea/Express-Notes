fe20bace04477b2ee7bced4f4ef5baadb479d109
Patrick O'Dea <po39dea@gmail.com>
2023-10-05 21:56:45 -0600

    Set up get requests for each page

--- server.js
+++ server.js
@@ -7,9 +7,12 @@ const PORT = 3001;
 app.use(express.static("public"));
 
 app.get("/", (req, res) =>
- res.sendFile(path.join())
-
+res.sendFile(path.join())
 )
+
+app.get("/notes", (req, res) =>
+res.sendFile(path.join(__dirname, "public/notes.html"))
+);
 
 app.listen(PORT, () => 
 console.log("hello")
