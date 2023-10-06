c9b85fdf7a55e6cbc184449b343568d5cc602d57
Patrick O'Dea <po39dea@gmail.com>
2023-10-05 22:49:34 -0600

    Setting up note api GET request

--- server.js
+++ server.js
@@ -1,20 +1,31 @@
 const express = require("express");
 const path = require("path");
+const termData = require("./api/notes")
+const PORT = 3001;
 
 const app = express();
-const PORT = 3001;
 
 app.use(express.static("public"));
 
-app.get("/", (req, res) =>
-res.sendFile(path.join())
-)
 
+//Calling Index.html (Homepage)
+app.get("*", (req, res) =>
+res.send()
+);
+
+
+//API?
+
+app.get("/api/notes", (req, res) => res.json(termData));
+
+//GET note route
 app.get("/notes", (req, res) =>
 res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
-app.listen(PORT, () => 
-console.log("hello")
 
+
+//Sending to PORT
+app.listen(PORT, () => 
+console.log(`SERVER LISTENING ON PORT ${PORT}`)
 );
