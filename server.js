9d20facc983d88b2bbb66ca15cd771426d303333
Patrick O'Dea <po39dea@gmail.com>
2023-10-08 18:05:29 -0600

    Set up post request

--- server.js
+++ server.js
@@ -1,12 +1,15 @@
 const express = require("express");
 const path = require("path");
-//const termData = require("./api/notes")
+const noteData = require("./db/db.json");
+
+
 const PORT = 3001;
 
 const app = express();
 
 app.use(express.static("public"));
-
+app.use(express.json ());
+app.use(express.urlencoded({ extended: true }));
 
 //Calling Index.html (Homepage)
 app.get("*", (req, res) =>
@@ -14,16 +17,29 @@ res.send()
 );
 
 
-//API?
-
-//app.get("/api/notes", (req, res) => res.json(termData));
-
 //GET note route
 app.get("/notes", (req, res) =>
 res.sendFile(path.join(__dirname, "public/notes.html"))
 );
 
+//API?
 
+app.get("/api/notes", (req, res) => res.json(noteData));
+
+app.post("/api/notes", (req, res) => {
+    let response;
+
+    if (req.body && req.body.title){
+        response = {
+            status: "success",
+            data: req.body,
+        };
+        res.json(`Note Title ${response.data.title} has been added!`);
+    } else{
+        res.json (`Note Title not found`);
+    }
+    console.log(req.body);
+});
 
 //Sending to PORT
 app.listen(PORT, () => 
